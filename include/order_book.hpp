c645b9a23ab9c42ba5210c31c4467018310c6cd3
Adarsh Rajesh <adarshrajesh.one@gmail.com>
2026-08-27 12:45:17 -0500

    preliminary implementation of engine

--- include/order_book.hpp
+++ include/order_book.hpp
@@ -7,7 +7,7 @@
 //a basic order book to my understand all it does is it takes in incoming bids and offers and sorts two lists or rather optimizes so that the highest bid and lowest offer at anytime can be called upon anytime in as less time as possible
 //to this end this we need insertion and the least in for offers and most for bids easily queryable and removable
 //a sorted set in c++ implements insertion, deletion in log(n) and querying the first element is O(1)
-//however beyond just those deleting orders by order id is needed so an ordered map will be used
+//however beyond just those deleting orders by order id is needed so an sorted map will be used
 
 //allowing an orderbook to be created with differnt ordering and adding quotes in and getting what is inside it next and also being able to remove what is next is all the funcitonality that should be needed
 
@@ -25,27 +25,41 @@
 
 class OrderBook {
 
-using OrderId = std::uint64_t;
-using Price =std::int64_t;
-using Quantity = std::uint32_t;
-
-struct Fill {
-    OrderId resting_id;
-    OrderId incoming_id;
-    Price price;
-    Quantity quantity;
-};
-private:
-    std::map<int, std::list<int>, std::greater<int>> bid;
-    std::map<int, std::list<int>> ask;
-    std::unordered_map<int, std::list<int>::iterator> orders;
 public:
+    using OrderId = std::uint64_t;
+    using Price =std::int64_t;
+    using Quantity = std::uint32_t;
+
+    struct Fill {
+        OrderId resting_id;
+        OrderId incoming_id;
+        Price price;
+        Quantity quantity;
+    };
+
+    enum class Side {
+        Buy,
+        Sell
+    };
+
+    struct Order {
+        OrderId id;
+        Price price;
+        Quantity quantity;
+        Side side;
+    };
+
     OrderBook();
-    std::vector<Fill> limit_buy(OrderId id, Price price, Quantity);
-    std::vector<Fill> limit_sell(OrderId id, Price price, Quantity);
+    std::vector<Fill> limit_buy(OrderId id, Price price, Quantity quantity);
+    std::vector<Fill> limit_sell(OrderId id, Price price, Quantity quantity);
     bool cancel(OrderId id);
     std::optional<Price> best_bid() const;
     std::optional<Price> best_ask() const;
+
+private:
+    std::map<int, std::list<Order>, std::greater<int>> bid;
+    std::map<int, std::list<Order>> ask;
+    std::unordered_map<int, std::list<Order>::iterator> orders;
 
 };
 #endif
--- src/order_book.cpp
+++ src/order_book.cpp
@@ -7,29 +7,142 @@
 #include <cstdint>
 #include <vector> 
 #include <functional> 
+#include <algorithm>
 
-class OrderBook {
+#include "order_book.hpp"
 
-using OrderId = std::uint64_t;
-using Price =std::int64_t;
-using Quantity = std::uint32_t;
 
-struct Fill {
-    OrderId resting_id;
-    OrderId incoming_id;
-    Price price;
-    Quantity quantity;
-};
-private:
-    std::map<int, std::list<int>, std::greater<int>> bid;
-    std::map<int, std::list<int>> ask;
-    std::unordered_map<int, std::list<int>::iterator> orders;
-public:
-    OrderBook();
-    std::vector<Fill> limit_buy(OrderId id, Price price, Quantity);
-    std::vector<Fill> limit_sell(OrderId id, Price price, Quantity);
-    bool cancel(OrderId id);
-    std::optional<Price> best_bid() const;
-    std::optional<Price> best_ask() const;
 
-};
+
+
+std::vector<OrderBook::Fill> OrderBook::limit_buy(OrderId id, Price price, Quantity quantity) {
+    if(orders.contains(id)){
+        return {};
+    }
+    std::vector<Fill> res;
+    while(quantity > 0 && !ask.empty()) {
+        Order cur = ask.begin()->second.front();
+        if (cur.price <= price) {
+            OrderBook::Fill record;
+            record.resting_id = cur.id;
+            record.incoming_id = id;
+            record.price = cur.price;
+            record.quantity = std::min(cur.quantity, quantity);
+            if(cur.quantity <= quantity) {
+                ask.begin()->second.pop_front();
+                if(ask[cur.price].empty()) {
+                    ask.erase(cur.price);
+                }
+                orders.erase(record.resting_id);
+            }
+            else {
+                ask.begin()->second.front().quantity -= quantity;
+            }
+            quantity -= record.quantity;
+            res.push_back(record);
+        }
+        else {
+            break;
+        }
+    }
+    if(quantity != 0) {
+        Order leftover;
+        leftover.id = id;
+        leftover.price = price;
+        leftover.quantity = quantity;
+        leftover.side = Side::Buy;
+        if(bid.contains(leftover.price)) {
+            bid[leftover.price].push_back(leftover);
+        }
+        else {
+            bid[leftover.price] = std::list<Order>{leftover};
+        }
+        orders[id] = std::prev(bid[leftover.price].end());
+    }
+    return res;
+}
+
+std::vector<OrderBook::Fill> OrderBook::limit_sell(OrderId id, Price price, Quantity quantity) {
+      if(orders.contains(id)){
+        return {};
+    }
+    std::vector<Fill> res;
+    while(quantity > 0 && !bid.empty()) {
+        Order cur = bid.begin()->second.front();
+        if (cur.price >= price) {
+            OrderBook::Fill record;
+            record.resting_id = cur.id;
+            record.incoming_id = id;
+            record.price = cur.price;
+            record.quantity = std::min(cur.quantity, quantity);
+            if(cur.quantity <= quantity) {
+                bid.begin()->second.pop_front();
+                if(bid[cur.price].empty()) {
+                    bid.erase(cur.price);
+                }
+                orders.erase(record.resting_id);
+            }
+            else {
+                bid.begin()->second.front().quantity -= quantity;
+            }
+            quantity -= record.quantity;
+            res.push_back(record);
+        }
+        else {
+            break;
+        }
+    }
+    if(quantity != 0) {
+        Order leftover;
+        leftover.id = id;
+        leftover.price = price;
+        leftover.quantity = quantity;
+        leftover.side = Side::Sell;
+        if(ask.contains(leftover.price)) {
+            ask[leftover.price].push_back(leftover);
+        }
+        else {
+            ask[leftover.price] = std::list<Order>{leftover};
+        }
+        orders[id] = std::prev(ask[leftover.price].end());
+    }
+    return res;
+}
+
+bool OrderBook::cancel(OrderId id) {
+    if(orders.contains(id)) {
+        std::list<Order>::iterator iter = orders[id];
+        Order cur = *iter;
+        if(cur.side == Side::Sell) {
+            ask[cur.price].erase(iter);
+            if(ask[cur.price].empty()) {
+                ask.erase(cur.price);
+            }
+        }
+        else {
+            bid[cur.price].erase(iter);
+            if(bid[cur.price].empty()) {
+                bid.erase(cur.price);
+            }
+        }
+        orders.erase(id);
+        return true;
+    }
+    return false;
+}
+
+std::optional<OrderBook::Price> OrderBook::best_bid() const {
+    if(!bid.empty()) {
+        return bid.begin()->first;
+    }
+    return std::nullopt;
+
+}
+
+std::optional<OrderBook::Price> OrderBook::best_ask() const {
+    if(!ask.empty()) {
+        return ask.begin()->first;
+    }
+    return std::nullopt;
+}
+
